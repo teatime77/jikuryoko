e88cb5d1464eebd25794645a168445e0a9647fb1
teatime77 <teatime77@live.jp>
2019-10-16 19:40:51 +0900

    draw.tsを追加。 PackageParameter => Package

---
+++ ts/draw.ts
@@ -0,0 +1,29 @@
+import { Drawable, GPGPU, TextureInfo, Package }  from "./lib/gpgpu.js";
+import { makePlaneBuffers, Box } from  "./lib/shape.js";
+
+export let mapBox: Box = new Box(-2, 2, -2, 2, 0, 0);
+export class CanvasDrawable extends Drawable {
+    canvas: HTMLCanvasElement;
+
+    constructor(canvas: HTMLCanvasElement) {
+        super();
+        this.canvas = canvas;
+    }
+
+    getParam() {
+        if (!this.param) {
+
+            var [mesh, idx_array] = makePlaneBuffers(mapBox, 11, 11, new TextureInfo(null, null, this.canvas));
+
+            this.param = {
+                id: "Earth",
+                vertexShader: GPGPU.textureSphereVertexShader,
+                fragmentShader: GPGPU.defaultFragmentShader,
+                args: mesh,
+                VertexIndexBuffer: idx_array
+            } as any as Package;
+        }
+
+        return this.param;
+    }
+}
